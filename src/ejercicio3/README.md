18ad8e4039f5037266e80954dd11803cdcef3715
Laia <laia@hidalgojimenez.es>
2021-12-09 17:02:13 +0100

    added exercise 3

---
+++ src/ejercicio3/README.md
@@ -0,0 +1,6 @@
+En este ejercicio tenéis que crear un bucle que permita concatenar 
+textos e imprima el resultado final por consola.
+
+Tened en cuenta que los textos pueden venir de un array de tipo String.
+
+Por ejemplo: String[] nombres = {"", "", "", “”} HECHO
---
+++ src/ejercicio3/ejercicio3.java
@@ -0,0 +1,12 @@
+package ejercicio3;
+
+public class ejercicio3 {
+
+    public static void main(String[] args) {
+
+        String[] nombres = {"laia", "ernesto", "fran"};
+
+        for (int i = 0; i < nombres.length; i++)
+            System.out.print(nombres[i] + " ");
+    }
+}

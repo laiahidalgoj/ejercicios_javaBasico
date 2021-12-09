a9288d37e6b615a678485fe105ff803795f8f397
Laia <laia@hidalgojimenez.es>
2021-12-09 16:39:53 +0100

    added java exercises

---
+++ src/ejercicio1/ejercicio1.java
@@ -0,0 +1,22 @@
+package ejercicio1;
+
+public class ejercicio1 {
+
+    public static void main(String[] args) {
+
+        int numero = 5;
+        System.out.println(numero);
+
+        double numDoble = 5.5;
+        System.out.println(numDoble);
+
+        long numLong = 535;
+        System.out.println(numLong);
+
+        boolean boleano = true;
+        System.out.println(boleano);
+
+        String nombre = "Laia";
+        System.out.println(nombre);
+    }
+}
